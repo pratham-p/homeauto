95b08bbafb98da7d41ec20d93f217a51f4dd914d
Prathmesh <pats2487@gmail.com>
2016-05-15 02:32:15 -0700

    mobile friendly menu changes

--- smartlights/app/static/js/script.js
+++ smartlights/app/static/js/script.js
@@ -28,3 +28,10 @@ $(function () {
         toggleOutlet($(this));
     });
 });
+
+$(document).ready(function(){
+	$(".nav a").on("click", function(){
+		$(".nav").find(".active").removeClass("active");
+		$(this).parent().addClass("active");
+	});
+});
